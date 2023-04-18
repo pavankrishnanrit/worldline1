f971937be0902e1c61fcfb90b5485dc95d29e6a3
unknown <pavankrishnanr.it2020@citchennai.net>
2023-04-18 22:01:15 +0530

    second

--- db.js
+++ db.js
@@ -2,7 +2,7 @@
 const mysql = require("mysql2/promise");
 const db = mysql.createConnection({
   host: "localhost",
-  user: "root",
+  user: "admin",
   password: "pavan1982",
   database: "register",
 });
